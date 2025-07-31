a9226dfa7f0f8b2456b8be79440daf456b692497
Dylan Callaghan <21831599@sun.ac.za>
2025-07-31 11:41:59 +0200

    Updated requirements

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 source ./.venv/bin/activate
-pip install pytest-cov
-pip install coverage-sbfl
-timeout 2h pytest -W ignore::DeprecationWarning --disable-pytest-warnings --cov=. --cov-context=test --timeout=300 --continue-on-collection-errors -ra > coverage_output.txt
+echo "Running tests..."
+timeout 2h pytest -W ignore::DeprecationWarning --disable-pytest-warnings --cov=. --cov-context=test --timeout=300 --continue-on-collection-errors -ra | tee coverage_output.txt
+echo "Forming spectrum..."
 coverage-sbfl -e coverage_output.txt
+echo "Done!"
 deactivate
